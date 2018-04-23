6665673a36593d59e3a14499754990cccc8c2ce9
scarlett07 <scarlett.geo@gmail.com>
2018-04-23 13:16:46 -0500

    Font awesome

---
+++ src/Pages/NewsFeedPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+export default class Newsfeed extends Component {
+    constructor(props) {
+        super(props);
+        
+
+    }//END Constructor
+
+    
+
+    render() {
+        return (
+            <div>
+              Página de newsfeed WIP
+            </div>)
+    }
+} //END newsfeed
