331a8b334c0a4ae749929440e6a3a7a11619be87
scarlett07 <scarlett.geo@gmail.com>
2018-04-23 18:10:37 -0500

    Post

--- src/Pages/NewsFeedPage.js
+++ src/Pages/NewsFeedPage.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 
-//Components
-import InputPost from "./Components/InputPost"
+//Elements
+import InputPost from './Components/InputPost';
+import Post from './Components/Post';
 
 
 
-export default class Newsfeed extends Component {
-   
+class Newsfeed extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChangeInput = this.handleChangeInput.bind(this);
 
-    
+        this.state = {
+            messages: [],
+            text: ""
+        }
+
+    }//END Constructor
+
+    handleClick() {
+        let message = { id: Date.now(), text: this.state.text };
+
+        this.setState(
+            {
+                messages: this.state.messages.concat([message]),
+                text: ""
+            }
+        );
+    } //EDN submit and clear input
+
+    handleChangeInput(value) {
+        this.setState({ text: value })
+    }
 
     render() {
-
         return (
             <div>
-              Página de newsfeed WIP
-              <InputPost 
-              state={this.props.state}
-              handleSubmit = {this.props.handleSubmit}
-              handleChangeInput = {this.props.handleChangeInput}
-              />
+                <InputPost
+                    handleClick={this.handleClick}
+                    handleChangeInput={this.handleChangeInput}
+                    text={this.state.text}
+                />
+                <Post
+                    messages={this.state.messages}
+                />
             </div>)
     }
 } //END newsfeed
+
+export default Newsfeed;
