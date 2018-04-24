8ab45807bf4f1d3f4331220fdd4141f6dec66a8c
scarlett07 <scarlett.geo@gmail.com>
2018-04-24 02:59:55 -0500

    Final Post

--- src/Pages/NewsFeedPage.js
+++ src/Pages/NewsFeedPage.js
@@ -15,11 +15,13 @@ class Newsfeed extends Component {
         this.handleChangeInput = this.handleChangeInput.bind(this);
 
         this.state = {
+            user:"",
             posts: [],
             text: "",
             messagesUser: [] ,
-            keyPhrases:{}
-            
+            keyPhrases:{},
+            score:{} ,
+            postFinal:{}           
         }
 
     }//END Constructor
@@ -29,15 +31,14 @@ class Newsfeed extends Component {
                          id: Date.now(),
                          text: this.state.text };
 
-        let messages = 
         this.setState(
                 { messagesUser : this.state.messagesUser.concat([message]),
                   text: "" }
                 );//END submit and clear input
         
 
-            
-            fetch("http://api-worldnews.azurewebsites.net/cognitive/keyphrases", {
+        //Post keyphrases
+         fetch("http://api-worldnews.azurewebsites.net/cognitive/keyphrases", {
             method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -47,9 +48,49 @@ class Newsfeed extends Component {
         })
             .then((response) => response.json())
             .then(parsedJSON => this.setState({ keyPhrases: parsedJSON }))
-            .catch(error => console.log('parsing failed'))
+            .catch(error => console.log('parsing failed')) //END keyphrases
+
+        //Post Score
+         fetch("http://api-worldnews.azurewebsites.net/cognitive/sentiment", {
+            method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ documents: [message] }),
+            "content-type": 'application/json',
+        })
+            .then((response) => response.json())
+            .then(parsedJSON => this.setState({ score: parsedJSON }))
+            .then(()=>this.creatingfinalPost())
+            .catch(error => console.log('parsing failed')) //END Score
+       
             
-    } 
+    } //END handleclick 
+
+    creatingfinalPost(){
+
+        let index=this.state.messagesUser.length;
+        console.log(this.state.keyPhrases.documents);
+        
+               
+
+        const finalPost = {
+            user: this.state.user,
+            text: this.state.messagesUser[index-1].text,
+            metaData: {
+                keyPhrases: this.state.keyPhrases.documents[0].keyPhrases,
+                sentiment: this.state.score.documents[0].score,
+                photos: [],
+                video: []
+            }
+        };
+            
+        this.setState({ postFinal: finalPost });
+
+        
+        
+    } //END creatingfinalPost and POST FINALPOST
+
 
     handleChangeInput(value) {
         this.setState({ text: value })
@@ -72,7 +113,11 @@ class Newsfeed extends Component {
     }// END fetchData
 
     render() {
-        console.log({ documents: this.state.keyPhrases } );
+        console.log("keyphrasee", this.state.keyPhrases );
+        console.log( "score",  this.state.score );
+        console.log(this.state.postFinal);
+        
+        
         
         return (
             <div>
