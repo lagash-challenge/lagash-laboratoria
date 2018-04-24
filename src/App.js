2ed79365da7fb96888d5038eb27c9359b5a26072
CruzMayra <velazquez.leonora@gmail.com>
2018-04-24 04:11:48 -0500

    post

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import React, { Component } from 'react';
+import {Switch, Route, Router} from 'react-router';
 import './App.css';
-import './Pages/NewsFeedPage'
 
 //Pages
 import Newsfeed from './Pages/NewsFeedPage';
+import Dashboard from'./Pages/DashboardPage';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-   // this.handleClick = this.handleClick.bind(this);
-    //this.handleChangeInput = this.handleChangeInput.bind(this);
-
-    this.state = {
-      user:"",
-      messages: [],
-      text: "",
-      post:[]
-    }
-
-  }//END Constructor
-
-  componentWillUnmount() {
-    
-
-  } // END componentWillUnmount
-
-  handleSubmit(user){
-    let message = { id: Date.now(), text: this.state.text };
-
-    this.setState(
-      {
-        user: user,
-        messages: this.state.messages.concat([message]),
-        text: ""
-      }
-    );
-  } //END handleSubmit
-
-  handleChangeInput(value) {
-    this.setState({ text: value })
-  }
-
-
-
   render() {
-    console.log(this.state.post);
-    
     return (
       <div className="App">
-        <Newsfeed 
-        state = {this.state}
-        handleSubmit = {this.handleSubmit}
-        handleChangeInput = {this.handleChangeInput}
-        />
+      <Router>
+            <Route exact path="/" component={Newsfeed}/>
+            <Route exact path="/dashboard" component={Dashboard}/>
+        </ Router>
       </div>
     );
   }
