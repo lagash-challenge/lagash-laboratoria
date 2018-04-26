1aa86c0f84d4a85f1a9e489b56889290e46c0f96
CruzMayra <velazquez.leonora@gmail.com>
2018-04-26 12:14:08 -0500

    creación de taps para demo

--- src/Pages/DashboardPage.js
+++ src/Pages/DashboardPage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
 //Components
-import Menu from './Components/Navbar';
 
 export default class Dashboard extends Component {
     render() {
       return (
         <div>
-          <Menu />
+          Aquí van las bonitas gráficas.
         </div>
       );
     }
